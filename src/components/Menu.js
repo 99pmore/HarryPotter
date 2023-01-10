d79ad085123a07dc21b5f8a0de9accae84deccd1
99pmore <morenomartin.pablo@gmail.com>
2023-01-10 23:30:38 +0100

    underline animation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Menu() {
 
@@ -7,19 +9,19 @@ export default function Menu() {
             <nav className="navbar-container">
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/"><FontAwesomeIcon icon={faHome} /></Link>
                     </li>
                     <li>
-                        <Link to="/Gryffindor">Gryffindor</Link>
+                        <Link className="hover-underline-animation" to="/Gryffindor">Gryffindor</Link>
                     </li>
                     <li>
-                        <Link to="/Slytherin">Slytherin</Link>
+                        <Link className="hover-underline-animation" to="/Slytherin">Slytherin</Link>
                     </li>
                     <li>
-                        <Link to="/Hufflepuff">Hufflepuff</Link>
+                        <Link className="hover-underline-animation" to="/Hufflepuff">Hufflepuff</Link>
                     </li>
                     <li>
-                        <Link to="/Ravenclaw">Ravenclaw</Link>
+                        <Link className="hover-underline-animation" to="/Ravenclaw">Ravenclaw</Link>
                     </li>
                 </ul>
             </nav>
