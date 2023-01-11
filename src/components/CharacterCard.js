ff42bb1af7e111017ec21b144183a468896210de
99pmore <morenomartin.pablo@gmail.com>
2023-01-11 02:42:31 +0100

    link to character info

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,23 @@
+import { useNavigate  } from "react-router-dom";
+
 export const CharacterCard = ({ character }) => {
 
+    const navigate = useNavigate();
+
+    const toCharacter = () => {
+        navigate('/characters/${ character.name }', { state: { character } })
+    }
+
     return (
-        <div className="character-card">
-            <div className="card-img">
-                <img src={ character.image } alt={ character.name + ' image' } />
+        <>
+            <div className="character-card">
+                <div className="card-img">
+                    <img src={ character.image } alt={ character.name + ' image' } onClick={ toCharacter } />
+                </div>
+                <div className="card-info">
+                    <h2>{ character.name }</h2>
+                </div>
             </div>
-            <div className="card-info">
-                <h2>{ character.name }</h2>
-            </div>
-        </div>
+        </>
     )
 }
