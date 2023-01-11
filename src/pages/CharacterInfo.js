067f2c6dccea438a7338faae4cf44541b8e37ba7
99pmore <morenomartin.pablo@gmail.com>
2023-01-11 14:47:14 +0100

    components created for info labels

--- src/pages/CharacterInfo.js
+++ src/pages/CharacterInfo.js
@@ -1,4 +1,6 @@
 import { useLocation } from 'react-router-dom';
+import { CharacterDataLabels } from '../components/CharacterDataLabels'
+import { WandDataLabels } from '../components/WandDataLabels';
 
 export const CharacterInfo = () => {
 
@@ -8,29 +10,21 @@ export const CharacterInfo = () => {
     return (
         <main className="character-info">
             <div className="wrapper">
+
                 <div className="img-container">
                     <img src={ character.image } alt={ character.name + ' image' } />
                 </div>
+
                 <div className="info-container">
                     <h2>{ character.name }</h2>
+
                     <div className="info">
-                        { character.house ? <p>House: { character.house }</p> : null }
-                        { character.dateOfBirth ? <p>Birthday: { character.dateOfBirth }</p> : null }
-                        { character.gender ? <p>Gender: { character.gender }</p> : null }
-                        { character.species ? <p>Species: { character.species }</p> : null }
-                        { character.ancestry ? <p>Ancestry: { character.ancestry }</p> : null }
+                        <CharacterDataLabels character={ character } />
+                        <WandDataLabels wand={ character.wand } />
                     </div>
+
                 </div>
-                { character.wand.wood || character.wand.core || character.wand.length ?
-                            <div className="wand">
-                                <h2>Wand: </h2>
-                                <div className="wand-info">
-                                    { character.wand.wood ? <p>Wood: { character.wand.wood }</p> : null }
-                                    { character.wand.core ? <p>Core: { character.wand.core }</p> : null }
-                                    { character.wand.length ? <p>Length: { character.wand.length }</p> : null }
-                                </div>
-                            </div> 
-                : null }
+
             </div>
         </main>
     )
