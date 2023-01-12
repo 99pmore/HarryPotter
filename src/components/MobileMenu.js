1f6581396d1524b6176aee6efe23308e6c826f38
99pmore <morenomartin.pablo@gmail.com>
2023-01-12 13:04:06 +0100

    bar styled

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,8 +1,36 @@
+import { Outlet, Link } from "react-router-dom";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import gryffindor from "../assets/houses/gryffindor-light.webp"
+import slytherin from "../assets/houses/slytherin-light.webp"
+import hufflepuff from "../assets/houses/hufflepuff-light.webp"
+import ravenclaw from "../assets/houses/ravenclaw-light.webp"
+
 export const MobileMenu = () => {
 
     return (
-        <nav>
-            
-        </nav>
+        <>
+            <nav className="mobile-navbar-container">
+                <ul className="mobile-links">
+                    <li className="mobile-home">
+                        <Link to="/"><FontAwesomeIcon icon={ faHome } /></Link>
+                    </li>
+                    <li>
+                        <Link to="/Gryffindor"><img src={ gryffindor } alt="Gryffindor" /></Link>
+                    </li>
+                    <li>
+                        <Link to="/Slytherin"><img src={ slytherin } alt="Slytherin" /></Link>
+                    </li>
+                    <li>
+                        <Link to="/Hufflepuff"><img src={ hufflepuff } alt="Hufflepuff" /></Link>
+                    </li>
+                    <li>
+                        <Link to="/Ravenclaw"><img src={ ravenclaw } alt="Ravenclaw" /></Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </>
     )
 }
